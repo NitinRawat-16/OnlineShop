b13bff5321417cd6a63dc89d020b6be017d70194
Nitin Rawat <nitinr426@gmail.com>
2022-04-06 16:49:23 +0530

    Add price field in product

---
+++ DataLayer/AdminDll.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DataModelLayer;
+
+namespace DataLayer
+{
+    public class AdminDll
+    {
+        OnlineShop _context;
+        public AdminDll()
+        {
+            _context = new OnlineShop();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.Include(p=>p.ProductCategory).Include(p=>p.ProductSize).OrderBy(P=>P.ProductName).ToList();
+        }
+    }
+}
